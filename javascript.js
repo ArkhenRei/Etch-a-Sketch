ffe80195aef92d6146ede1d2cfbec269278aa89d
ArkhenRei <canitezyunus@gmail.com>
2024-04-26 15:51:28 +0300

    feat: Create mouse hover event
    When user hovers mouse on grid spaces their background changes.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,24 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    container.appendChild(row);
+for (let i = 0; i < 16; i++) {
+  //Creating 16 rows
+  const row = document.createElement("div");
+  row.className = "row";
+  //append rows to container
+  container.appendChild(row);
 
-    for(let i = 0; i < 16; i++) {
-        const col = document.createElement("div");
-        col.className = "col";
-        row.appendChild(col);
-    }
+  for (let i = 0; i < 16; i++) {
+    //creating 16 columns
+    const col = document.createElement("div");
+    col.className = "col";
+    //append columns to rows
+    row.appendChild(col);
+
+    col.addEventListener("mousemove", () => {
+      col.style.backgroundColor = "grey";
+    });
+    col.addEventListener("mouseout", () => {
+      col.style.backgroundColor = "white";
+    });
+  }
 }
-
